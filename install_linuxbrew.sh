e04eacf662076d0e2911e38ab8bd4fec58ed5d31
AruMaekawa <aru.maekawa@gmail.com>
2024-04-01 20:19:38 +0900

    update install_linuxbrew.sh

--- install_linuxbrew.sh
+++ install_linuxbrew.sh
@@ -13,14 +13,14 @@ if !(type brew > /dev/null 2>&1); then
    brew update --force --quiet
    chmod -R go-w "$(brew --prefix)/share/zsh"
 
+   echo 'export PATH="${HOME}/.linuxbrew/bin:$PATH"' >>~/.bashrc
+   source ~/.bashrc
+
 else
 
    echo "Linuxbrew is already installed."
 
 fi
-
-echo 'export PATH="${HOME}/.linuxbrew/bin:$PATH"' >>~/.bashrc
-source ~/.bashrc
 
 # check linuxbrew
 if !(type brew > /dev/null 2>&1); then

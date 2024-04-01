78687f6a2687b568094937c744085da69ce4fdd1
AruMaekawa <aru.maekawa@gmail.com>
2024-04-01 19:07:59 +0900

    update setup_bash.sh

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,27 +1,64 @@
+# Description: Setup bash environment
+
+# config files
+
+echo "Install config files ==========>"
+
+CUR_DIR="$(
+   cd $(dirname $0)
+   pwd
+)"
+
+# .bashrc (overwrite)
+if [ -f ~/.bashrc ]; then
+   mv ~/.bashrc ~/.bashrc.old
+fi
+
+echo "ln -nfs $CUR_DIR/files/.bashrc ~/"
+ln -nfs $CUR_DIR/files/.bashrc ~/
+
+# .tmux.conf
+if [ ! -f ~/.tmux.conf ]; then
+   echo "ln -nfs $CUR_DIR/files/.tmux.conf ~/"
+   ln -nfs $CUR_DIR/files/.tmux.conf ~/
+fi
+
+# .bash_aliases
+if [ ! -f ~/.bash_aliases ]; then
+   echo "ln -nfs $CUR_DIR/files/.bash_aliases ~/"
+   ln -nfs $CUR_DIR/files/.bash_aliases ~/
+fi
+
+echo "====================> Done!!"
+
 # pyenv
 
-echo "########## Install pyenv ##########"
+echo "Install pyenv ==========>"
 
-if [ ! -d ~/.pyenv ]; then
+if !(type pyenv > /dev/null 2>&1); then
+   # install from source
    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-   export PYENV_ROOT="$HOME/.pyenv"
-   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-   eval "$(pyenv init -)"
-
+   # add path to .bashrc
    echo '
-   # pyenv
-   export PYENV_ROOT="$HOME/.pyenv"
-   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-   eval "$(pyenv init -)"' >>~/.bashrc
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"' >>~/.bashrc
 
+   source ~/.bashrc
+
+else
+   echo "pyenv is already installed."
 fi
+
+echo "====================> Done!!"
 
 # anaconda
 
-echo "########## Install anaconda ##########"
+echo "Install anaconda ==========>"
 
-if [ -z "$(which conda)" ]; then
+if !(type conda > /dev/null 2>&1); then
    CONDA_VERSION=$(pyenv install -l | grep anaconda | tail -n 1 | sed 's/^ *\| *$//')
    echo Install $CONDA_VERSION
    pyenv install $CONDA_VERSION
@@ -31,46 +68,15 @@ if [ -z "$(which conda)" ]; then
    export CONDA_PATH=${HOME}/.pyenv/versions/${CONDA_VERSION}
    source \${CONDA_PATH}/etc/profile.d/conda.sh
    export PATH=\"\$CONDA_PATH/bin:\$PATH\"" >>~/.bashrc
+else
+   echo "anaconda is already installed."
 fi
 
-# dotfiles
+echo "===================> Done!!"
 
-echo "########## Install dotfiles ##########"
+# github ssh-key
 
-CUR_DIR="$(
-   cd $(dirname $0)
-   pwd
-)"
-
-if [ -f ~/.bashrc ]; then
-   mv ~/.bashrc ~/.bashrc.old
-fi
-
-echo "ln -nfs $CUR_DIR/files/.bashrc ~/"
-ln -nfs $CUR_DIR/files/.bashrc ~/
-
-if [ ! -f ~/.tmux.conf ]; then
-   echo "ln -nfs $CUR_DIR/files/.tmux.conf ~/"
-   ln -nfs $CUR_DIR/files/.tmux.conf ~/
-fi
-
-if [ ! -f ~/.bash_aliases ]; then
-   echo "ln -nfs $CUR_DIR/files/.bash_aliases ~/"
-   ln -nfs $CUR_DIR/files/.bash_aliases ~/
-fi
-
-# fzf
-
-echo "########## Install fzf ##########"
-
-if [ -z "$(which fzf)" ]; then
-   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-   ~/.fzf/install
-
-   echo "export FZF_DEFAULT_OPTS='--height 40% --reverse --border'" >>~/.bashrc
-fi
-
-echo "########## Install github ssh-key ##########"
+echo "Install github ssh-key ==========>"
 
 if [ ! -f ~/.ssh/id_ed25519 ]; then
    ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -C ""
@@ -80,8 +86,13 @@ if [ ! -f ~/.ssh/id_ed25519 ]; then
          Port 22
          HostName github.com
          IdentityFile ~/.ssh/id_ed25519" >~/.ssh/config
+
+else
+   echo "github ssh-key already exists."
 fi
 
 echo "~/.ssh/id_ed25519.pub: "
 cat ~/.ssh/id_ed25519.pub
 echo visit https://github.com/settings/ssh/new to add this ssh-key
+
+echo "=========================> Done!!"
